5c4be38aa02ffc890a9f49559ed27abcc72b9640
PabloGrajeda <grajeda.gragon@gmail.com>
2018-10-10 17:59:36 -0500

    -Se corrigio el problema de carga de imagenes
    -Ya se crea el array de las peliculas agregadas

--- src/MovieList/Movie/Movie.js
+++ src/MovieList/Movie/Movie.js
@@ -1,5 +1,5 @@
 // components import
-import React, { Component } from "react";
+import React from "react";
 import TitleMovie from "./TitleMovie/Titlemovie";
 import TitleCategory from "./TitleCategory/Titlecategory";
 import Detail from "./Detail/Detail";
@@ -9,44 +9,24 @@ import Image from "./Image/image";
 // css import
 import "./Movie.css"
 
-class Movie extends Component{
+const Movie = (props) =>{
 
-    constructor(props){
-        super(props);
-        this.state = {
-            movies: []
-        }
-    }
-
-    handleClick (event, addPelicula) {
-
-        this.state.movies.push(addPelicula);      
-        this.setState(
-            this.state
-          )
-
-    }
-
-
-    render(){
-        return(
-            
-            <div className="card">
-                <div className="imgcard">
-                    <Image img={this.props.img}></Image>
-                </div>
-                <div className="card-details">
-                    <TitleMovie title={this.props.title}></TitleMovie>
-                    <TitleCategory genre={this.props.genre}></TitleCategory>
-                    <div>
-                        <div className="options"> <Detail></Detail>  </div>
-                        <div className="options" onClick={(e) => this.handleClick(e,this.props.title)}> <Favorite></Favorite> </div>
-                    </div>
+   return (
+        <div className="card">
+            <div className="imgcard">
+                <Image img={props.img}></Image>
+            </div>
+            <div className="card-details">
+                <TitleMovie title={props.title}></TitleMovie>
+                <TitleCategory genre={props.genre}></TitleCategory>
+                <div>
+                    <div className="options"> <Detail></Detail>  </div>
+                    <div className="options" onClick={ (e, title) => props.onClick(e, props.title) }> <Favorite></Favorite> </div>
                 </div>
             </div>
-            
-        )
-    }
+        </div>
+   ) 
+
 }
 
 export default Movie;
--- src/MovieList/Movielist.js
+++ src/MovieList/Movielist.js
@@ -1,11 +1,25 @@
 // components import
 import React, { Component } from "react";
 import Movie from "./Movie/Movie"
+import Sidebar from "../SideBar/Sidebar";
 
 // css import
 import "./Movielist.css"
 
 class Movielist extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            movielist: []
+        }
+    }
+
+    handleClickAddFavorites = (e, title) => {
+        e.preventDefault();
+        this.setState({ movielist: this.state.movielist.concat( title ) })
+        return (<Sidebar prueba = {this.state.movielist} />) 
+    }
 
     render(){
         return(
@@ -13,20 +27,28 @@ class Movielist extends Component {
                 {/* {require('./responsive.jpg') */}
 
                 <div className="movielist">
-                    <Movie img="images/deadpool.jpeg" title="Como ser un Latin Lover" genre="Comedia" ></Movie>
-                    <Movie img="../../../Images/rampage.jpeg" title="Rampage" genre="Acción, Aventura" ></Movie>
-                    <Movie img="../../../Images/tom-raider.jpeg" title="Tom Raider" genre="Acción, Aventura" ></Movie>
-                    <Movie img="../../../Images/next-gen.jpeg" title="Next Gen" genre="Animacion, Acción, Aventura" ></Movie>
+                    <Movie onClick={(e, title) =>this.handleClickAddFavorites(e, title)} img="Images/latin-lover.jpeg" title="Como ser un Latin Lover" genre="Comedia" ></Movie>
+                    <Movie onClick={(e, title) =>this.handleClickAddFavorites(e, title)} img="Images/rampage.jpeg" title="Rampage" genre="Acción, Aventura" ></Movie>
+                    <Movie onClick={(e, title) =>this.handleClickAddFavorites(e, title)} img="Images/tom-raider.jpeg" title="Tom Raider" genre="Acción, Aventura" ></Movie>
+                    <Movie onClick={(e, title) =>this.handleClickAddFavorites(e, title)} img="Images/next-gen.jpeg" title="Next Gen" genre="Animacion, Acción, Aventura" ></Movie>
                 </div>
                
                 <div className="movielist">
-                    <Movie img="../../../deadpool.jpeg" title="Deadpool" genre="Accion, Ciencia Ficción, Superhéroes"></Movie>
-                    <Movie img="../../../ant-man.jpeg" title="Ant-Man" genre="Accion, Ciencia Ficción, Superhéroes"></Movie>
-                    <Movie img="../../../wonder-woman.jpeg" title="Wonder Woman" genre="Accion, Fantasia, Aventura"></Movie>
-                    <Movie img="../../../mission-impossible.jpeg" title="MIssion Impossible" genre="Cine de espionaje"></Movie>
+                    <Movie onClick={(e, title) =>this.handleClickAddFavorites(e, title)} img="Images/deadpool.jpeg" title="Deadpool" genre="Accion, Ciencia Ficción, Superhéroes"></Movie>
+                    <Movie onClick={(e, title) =>this.handleClickAddFavorites(e, title)} img="Images/ant-man.jpeg" title="Ant-Man" genre="Accion, Ciencia Ficción, Superhéroes"></Movie>
+                    <Movie onClick={(e, title) =>this.handleClickAddFavorites(e, title)} img="Images/wonder-woman.jpeg" title="Wonder Woman" genre="Accion, Fantasia, Aventura"></Movie>
+                    <Movie onClick={(e, title) =>this.handleClickAddFavorites(e, title)} img="Images/mission-impossible.jpeg" title="MIssion Impossible" genre="Cine de espionaje"></Movie>
+
+                    <ul>
+                           {this.state.movielist.map( (movie, index) => <li key={index}> {movie}  </li> )}
+                    </ul>
                 </div>
 
+
+
+
            </div>
+                        
         )
     }
 
